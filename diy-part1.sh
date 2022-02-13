8e8a864accd33492bd910510660cedaefb3dca25
MXJ <53927877+MXJNZ6@users.noreply.github.com>
2022-02-13 19:25:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,12 +29,10 @@ svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser pack
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant package/luci-app-fileassistant
 #git clone https://github.com/garypang13/luci-app-eqos package/luci-app-eqos
 #git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
-#git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+rm -rf package/lean/aliyundrive-webdav
+rm -rf package/lean/luci-app-aliyundrive-webdav
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/luci-app-aliyundrive-webdav
-git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
-#git clone https://github.com/ZhanhuaJin/luci-app-pptp-server package/luci-app-pptp-server
 git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 rm -rf package/lean/luci-app-dockerman
