4497794a6b8339e89e788420ceb9239b5c86ba09
MXJ <53927877+MXJNZ6@users.noreply.github.com>
2022-02-16 23:37:26 +0800

    Create delete.sh

---
+++ delete.sh
@@ -0,0 +1,8 @@
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/packages/net/aliyundrive-webdav
+rm -rf feeds/luci/applications/luci-app-dockerman
+rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+rm -rf feeds/packages/multimedia/UnblockNeteaseMusic
+rm -rf feeds/packages/multimedia/UnblockNeteaseMusic-Go
+rm -rf feeds/luci/applications/luci-app-unblockmusic
+rm -rf feeds/luci/applications/luci-app-openvpn-server
