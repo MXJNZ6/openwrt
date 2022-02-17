8e145754945f378b46a9c7b900c5b998aba7e19f
MXJ <53927877+MXJNZ6@users.noreply.github.com>
2022-02-17 14:11:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,6 +2,7 @@
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/packages/net/aliyundrive-webdav
 rm -rf feeds/luci/applications/luci-app-ocserv
+mv package/luci-app-ocserv feeds/luci/applications
 rm -rf feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/packages/multimedia/UnblockNeteaseMusic
 rm -rf feeds/luci/applications/luci-app-unblockmusic
