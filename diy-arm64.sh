90071f14033647a78784a0807a4f8a6ded68ba9e
MXJ <53927877+MXJNZ6@users.noreply.github.com>
2022-05-18 18:46:29 +0800

    Update diy-arm64.sh

--- diy-arm64.sh
+++ diy-arm64.sh
@@ -62,7 +62,7 @@ popd
 # 删除重复包
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/packages/multimedia//aliyundrive-webdav
+rm -rf feeds/packages/multimedia/aliyundrive-webdav
 #rm -rf feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/packages/multimedia/UnblockNeteaseMusic
 rm -rf feeds/luci/applications/luci-app-unblockmusic
